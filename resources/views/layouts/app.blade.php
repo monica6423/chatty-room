0214a7acab4d5fc42911c9d374aa5288f3c50a3e
monica6423 <monica6423@gmail.com>
2021-01-19 14:41:00 +0200

    setup postgres and run migration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chatty</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
-<body>
+<body class="bg-gray-100">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <ul class="flex items-center">
+            <li>
+                <a href="/" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Posts</a>
+            </li>
+        </ul>
+        <ul class="flex items-center">
+            <li><a href="" class="p-3">momo</a></li>
+            <li><a href="" class="p-3">Login</a></li>
+            <li><a href="" class="p-3">Register</a></li>
+            <li><a href="" class="p-3">Logout</a></li>
+        </ul>
+    </nav>
 <!-- inject content -->
     @yield('content')
 </body>
